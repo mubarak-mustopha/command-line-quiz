a1e3d1c1c0def6bcaa0c4e00cce3731265aab5af
Mustopha Mubarak <mustophamubarack17@gmail.com>
2023-10-28 01:03:11 +1100

    New regex tested on pages 32(containing german questions) and 47(conatining multiple choice questions)

--- generate-json.py
+++ generate-json.py
@@ -4,22 +4,36 @@ import pdfplumber
 
 def get_match_string(fullstring,match):
     """
-    Args: match --> match object,fullstring
-    return_value --> match string from fullstring 
+    A function that takes a string and a regex match object,
+    then return the matched substring from the string
     """
     return fullstring[match.start(): match.end()]
+
+    
 
 PATTERN = re.compile("(\d+[.].*?)\n([a-d][.][a-zA-Z+!? ]+\n?)",re.DOTALL)
 QUESTION_LIST = []#a list of {"question":abc,"answer":def}
 
 with pdfplumber.open("gns106+++.pdf") as gns_pdf:
-    german_pqs = gns_pdf.pages[30:]#list of pdf pages starting from 30 coz german question starts here.
-    for page in german_pqs:            
-        match = PATTERN.findall(page.extract_text())
-        QUESTION_LIST.extend([{"question":m[0],"answer":m[1]} for m in match])
+    pages = gns_pdf.pages[30:]#list of pdf pages starting from 30 coz german question starts here.
+    # for page in german_pqs:            
+    #     match = PATTERN.findall(page.extract_text())
+    #     QUESTION_LIST.extend([{"question":m[0],"answer":m[1]} for m in match])
+    page_32 = pages[1].extract_text()
+    matches_32 = [get_match_string(page_32,match_obj) for match_obj in PATTERN.finditer(page_32)]
+    print(page_32)
+    print(matches_32)
+    print("*"*150)
+    page_47 = pages[16].extract_text()
+    matches_47 = [get_match_string(page_47,match_obj) for match_obj in PATTERN.finditer(page_47)]
+    print(page_47)
+    print("*"*150)
+    print(matches_47)
 
-print(f"There're {len(QUESTION_LIST)} questions available.")
-print(QUESTION_LIST[:10])        
+    
 
-with open("gns-pq.json", "w") as gns_json:
-    json.dump(QUESTION_LIST,gns_json, indent=8, separators=(',\n',': '))
+# print(f"There're {len(QUESTION_LIST)} questions available.")
+# print(QUESTION_LIST[:10])        
+
+# with open("gns-pq.json", "w") as gns_json:
+#     json.dump(QUESTION_LIST,gns_json, indent=8, separators=(',\n',': '))
