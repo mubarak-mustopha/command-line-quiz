dbb624c84a1b090a56e8f4cf73b1aa065a9f9976
mubaarock <mustophamubarack17@gmail.com>
2023-12-12 22:12:06 +1100

    ORGANIZE CODE INTO FUNCTIONS AND MODIFY REGEX TO MATCH OPTIONS STARTING WITH CAPITAL `A-Z`
    Place PATTERN after import statements for easy access. Add `A-D` to
    character set matching for options to support options that start with
    capitals.
    
    Make the change above in the regex pattern in get_q_a_dict() function.
    
    Wrap code that parses questions from the pdf into new function
    parse_pdf().
    
    Create convert_to_json() function which takes a python object a writes
    it in a specified path in json format.

--- generate-json.py
+++ generate-json.py
@@ -2,6 +2,12 @@ import json
 import re
 import pdfplumber
 
+PATTERN = re.compile(r"""
+                     (\d+[.].*?)  
+                     \n
+                     ([A-Da-d][.][a-zA-Z+!?. ]+\n?){1,4}
+                     """,re.DOTALL|re.VERBOSE)
+
 def split_text(pattern, text):
     """
     Wrapper function for re.split
@@ -13,8 +19,8 @@ def get_q_and_a_dict(match_string):
     Match_string --> multiple choice/german question with options[1/4] 
     returns {question, option, answer}
     """
-    # split on the newline followed by a. which is the first option
-    question, options = split_text("\n(?=a[.])", match_string)
+    # split on the newline followed by a. or A. which is the first option
+    question, options = split_text("\n(?=[Aa][.])", match_string)
 
     #strip out question number
     question = re.sub("^\d+[. ]{1,2}","",question)
@@ -24,11 +30,11 @@ def get_q_and_a_dict(match_string):
     if len(options) == 1:
         answer = options[0]
 
-        #regex return "Muby" from "a. Muby"
+        #regex return "Muby" from "a. Muby"| "A. Muby"
         return {
             "question":question,
             "options": " ",
-            "answer": re.sub("^a[.] ","", answer)}
+            "answer": re.sub("^[Aa][.] ","", answer)}
     
     if len(options) != 4:
         return None
@@ -47,28 +53,49 @@ def get_q_and_a_dict(match_string):
     } 
 
 
-PATTERN = re.compile(r"""
-                     (\d+[.].*?)  
-                     \n
-                     ([a-d][.][a-zA-Z+!?. ]+\n?){1,4}
-                     """,re.DOTALL|re.VERBOSE)
+def parse_pdf(file_path ,pattern = PATTERN,page_start = 0, page_end = None  ,func = None):
+    """file_path: pdf-file path
 
-#a list of {"question":abc,"optoin":[]|" ","answer":def}
-QUESTION_LIST = []
+        pattern: text search pattern for pdf pages
 
-with pdfplumber.open("gns106+++.pdf") as gns_pdf:
-    #list of pdf pages starting from 30 coz non-tabular questions start here.
-    pages = gns_pdf.pages[30:]
+        page_start: page number to start search
 
-    for page in pages:
-        page_text = page.extract_text(x_tolerance = 1)
-        question_match_list = [match.group() 
-                                    for match in PATTERN.finditer(page_text)]
-        q_and_a_dict = [get_q_and_a_dict(match) for match in question_match_list]
-        QUESTION_LIST.extend(q_and_a_dict)
+        page_end: page number to end search
 
-print(f"There're {len(QUESTION_LIST)} questions available.")
-print(QUESTION_LIST[:10])     
+        func: function to transform all matched text in each page
 
-with open("gns-pq.json", "w") as gns_json:
-    json.dump(QUESTION_LIST,gns_json, indent=8, separators=(',\n',': '))
+        Returns a list of all matched text in every page
+    """
+
+    #a list of {"question":abc,"optoin":[]|" ","answer":def}
+    MATCH_LIST = []
+
+    with pdfplumber.open(file_path) as gns_pdf:
+        pages = gns_pdf.pages
+        pages_to_parse = pages[page_start:] if not page_end else pages[page_start:page_end]
+
+        for page in pages_to_parse:
+            page_text = page.extract_text(x_tolerance = 1)
+            page_match_list = [match.group() 
+                                        for match in pattern.finditer(page_text)]
+            
+            if not func:
+                MATCH_LIST.extend(page_match_list)
+            else:      
+                transformed_match_list = [func(match) for match in page_match_list]
+                MATCH_LIST.extend(transformed_match_list)
+
+    print(f"There're {len(MATCH_LIST)} questions available.")
+    print(MATCH_LIST[:10])     
+    return MATCH_LIST
+
+
+def convert_to_json(object, file_path):
+    """Turns a python object to a json file"""
+
+    with open(file_path, "w") as json_file:
+        json.dump(object, json_file , indent=8 , separators=(',\n',': '))
+
+
+convert_to_json(parse_pdf("gns106+++.pdf", PATTERN,page_start=30 ,func = get_q_and_a_dict),
+                 "gns-test.json")
